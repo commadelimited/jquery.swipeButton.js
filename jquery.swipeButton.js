5a6be4c38e899366d0aba26bd94ad02f61a1d60a
Lee Crossley <leee@hotmail.co.uk>
2012-03-07 14:33:16 +0000

    Button slide in left / right animation like a native control. Untested - but you get the idea.

--- jquery.swipeButton.js
+++ jquery.swipeButton.js
@@ -21,7 +21,9 @@
 				var $li = $(this);
 
 				// remove all currently displayed buttons
-				$('div.ui-btn, .' + o.btnClass, $parent).remove();
+				$('div.ui-btn, .' + o.btnClass, $parent).animate({ width: 'toggle' }, 150, function(e) {
+					$(this).remove();
+				});
 				
 				// create button
 				var $swipeBtn = $('<a>' + o.btnLabel + '</a>').attr({
@@ -32,8 +34,8 @@
 									'href': $li.data('swipeurl')
 								})
 								.bind('click tap', o.click);
-				// insert button into list item
-				$li.prepend($swipeBtn).find('.' + o.btnClass ).button();
+				// slide insert button into list item
+				$swipeBtn.button().hide().prependTo($li).animate({ width: 'toggle' }, 150);
 
 				// override row click
 				$('div a:not(' + o.btnClass + ')', $li).bind('click.swipe', function(e){
