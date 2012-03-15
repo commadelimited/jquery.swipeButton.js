b4ca9455a64d38c23930eb1f4d0944cc1b9ca61f
andy matthews <andy@commadelimited.com>
2012-03-15 12:39:33 -0500

    Removing button if swipe is performed on an active row
    Adding version numbers to files.
    Adding past releases folder for easy comparison.

---
+++ old/jquery.swipeButton-1.0.js
@@ -0,0 +1,65 @@
+/*
+	Name: jquery.swipeButton.js
+	Author: Andy andyMatthews
+	Website: http://andyMatthews.net
+	Packed With: http://jsutility.pjoneil.net/
+*/
+(function($){
+
+	$.fn.swipeDelete = function(o){
+		
+		o = $.extend( {}, $.fn.swipeDelete.defaults, o );
+		
+		return this.filter('[data-swipeurl]').each(function(i, el){
+
+			console.log(el);
+
+			var $e = $(el);
+			var $parent = $(el).parent('ul');
+
+			$e.bind(o.direction, function ( e ) {
+
+				// reference the current item
+				var $li = $(this);
+
+				// remove all currently displayed buttons
+				$('div.ui-btn, .' + o.btnClass, $parent).remove();
+				
+				// create button
+				var $swipeBtn = $('<a>' + o.btnLabel + '</a>').attr({
+									'data-role': 'button',
+									'data-inline': 'true',
+									'class': o.btnClass,
+									'data-theme': o.btnTheme,
+									'href': $li.data('swipeurl')
+								})
+								.bind('click tap', o.click);
+				// insert button into list item
+				$li.prepend($swipeBtn).find('.' + o.btnClass ).button();
+
+				// override row click
+				$('div a:not(' + o.btnClass + ')', $li).bind('click.swipe', function(e){
+					e.stopPropagation();
+					e.preventDefault();
+					$(this).unbind('click.swipe');
+					$li.removeClass('ui-btn-active').find('div.ui-btn').remove();
+				})
+
+			});
+
+		});
+		
+	}
+
+	$.fn.swipeDelete.defaults = {
+		direction: 'swiperight',
+		btnLabel: 'Delete',
+		btnTheme: 'e',
+		btnClass: 'aSwipeBtn',
+		click: function(e){
+			e.preventDefault();
+			$(this).parents('li').remove();
+		}
+	};
+
+})(jQuery);
