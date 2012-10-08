5291a72df6e7bc026f77e6196bf7b57cee262768
jeffijoe <jeffijoe@hotmail.com>
2012-10-08 16:29:47 +0300

    Update src/jquery.swipeButton.js
    Proposed alwaysRemove property, for removing the delete button regardless, when a click/tap occurs on it.

--- src/jquery.swipeButton.js
+++ src/jquery.swipeButton.js
@@ -1,72 +1,78 @@
 /*
-	Name: jquery.swipeButton.js
-	Author: Andy andyMatthews
-	Website: http://andyMatthews.net
-	Version: 1.2.1
+Name: jquery.swipeButton.js
+Author: Andy andyMatthews
+Website: http://andyMatthews.net
+Version: 1.2.2
 */
-(function($){
+(function ($) {
 
-	$.fn.swipeDelete = function(o){
+    $.fn.swipeDelete = function (o) {
 
-		o = $.extend( {}, $.fn.swipeDelete.defaults, o );
-		
-		return this.filter('[data-swipeurl]').each(function(i, el){
-			var $e = $(el);
-			var $parent = $(el).parent('ul');
+        o = $.extend({}, $.fn.swipeDelete.defaults, o);
 
-			$e.on(o.direction, function ( e ) {
+        return this.filter('[data-swipeurl]').each(function (i, el) {
+            var $e = $(el);
+            var $parent = $(el).parent('ul');
 
-				// reference the current item
-				var $li = $(this);
-				var cnt = $('.ui-btn', $li).length;
+            $e.on(o.direction, function (e) {
 
-				// remove all currently displayed buttons
-				$('div.ui-btn, .' + o.btnClass, $parent).animate({ width: 'toggle' }, 200, function(e) {
-					$(this).remove();
-				});
+                // reference the current item
+                var $li = $(this);
+                var cnt = $('.ui-btn', $li).length;
 
-				// if there's an existing button we simply delete it, then stop
-				if (!cnt) {
-					// create button
-					var $swipeBtn = $('<a>' + o.btnLabel + '</a>').attr({
-										'data-role': 'button',
-										'data-mini': true,
-										'data-inline': 'true',
-										'class': (o.btnClass === 'aSwipeBtn') ? o.btnClass : o.btnClass + ' aSwipeBtn',
-										'data-theme': o.btnTheme,
-										'href': $li.data('swipeurl')
-									})
-									.on('click tap', o.click);
+                // remove all currently displayed buttons
+                $('div.ui-btn, .' + o.btnClass, $parent).animate({ width: 'toggle' }, 200, function (e) {
+                    $(this).remove();
+                });
 
-					// slide insert button into list item
-					$swipeBtn.prependTo($li).button();
-					$li.find('.ui-btn').hide().animate({ width: 'toggle' }, 200);
+                // if there's an existing button we simply delete it, then stop
+                if (!cnt) {
+                    // create button
+                    var $swipeBtn = $('<a>' + o.btnLabel + '</a>').attr({
+                        'data-role': 'button',
+                        'data-mini': true,
+                        'data-inline': 'true',
+                        'class': (o.btnClass === 'aSwipeBtn') ? o.btnClass : o.btnClass + ' aSwipeBtn',
+                        'data-theme': o.btnTheme,
+                        'href': $li.data('swipeurl')
+                    }).on('vclick', o.click).one("vclick", function() {
+                        // Remove the button on click/tap if specified in the config
+                        if(o.alwaysRemove)
+                        $(this).animate({ width: "toggle" }, function() {
+                            $(this).remove();
+                        });
+                    });
 
-					// override row click
-					$('div a:not(' + o.btnClass + ')', $li).on('click.swipe', function(e){
-						e.stopPropagation();
-						e.preventDefault();
-						$(this).off('click.swipe');
-						$li.removeClass('ui-btn-active').find('div.ui-btn').remove();
-					});
+                    // slide insert button into list item
+                    $swipeBtn.prependTo($li).button();
+                    $li.find('.ui-btn').hide().animate({ width: 'toggle' }, 200);
 
-				}
+                    // override row click
+                    $('div a:not(' + o.btnClass + ')', $li).on('click.swipe', function (e) {
+                        e.stopPropagation();
+                        e.preventDefault();
+                        $(this).off('click.swipe');
+                        $li.removeClass('ui-btn-active').find('div.ui-btn').remove();
+                    });
+
+                }
 
 
-			});
+            });
 
-		});
-	};
+        });
+    };
 
-	$.fn.swipeDelete.defaults = {
-		direction: 'swiperight',
-		btnLabel: 'Delete',
-		btnTheme: 'e',
-		btnClass: 'aSwipeBtn',
-		click: function(e){
-			e.preventDefault();
-			$(this).parents('li').slideUp();
-		}
-	};
+    $.fn.swipeDelete.defaults = {
+        alwaysRemove:true,
+        direction: 'swiperight',
+        btnLabel: 'Delete',
+        btnTheme: 'e',
+        btnClass: 'aSwipeBtn',
+        click: function (e) {
+            e.preventDefault();
+            $(this).parents('li').slideUp();
+        }
+    };
 
-}(jQuery));
+} (jQuery));
